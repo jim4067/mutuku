19ba0ed1856a01eed0a9259484036818939c1942
jim4067 <jimmyimpulse2@gmail.com>
2021-10-14 14:23:14 +0300

    :sparkles: diplay random quote with every visit to the site

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 document.addEventListener('DOMContentLoaded', (event) => {
 
-    const the_quotes = ["I wanna love free even if that freedom hurts me. I want to decide my happiness for myself -  the promised neverland."]
+    const the_quotes = ["I wanna love free even if that freedom hurts me. I want to decide my happiness for myself -  the promised neverland.",
+        "If I always blame someone or something, i'll never change - fruits basket"
+    ]
+
+    //generate random quote
+    async function fetch_quote() {
+        const res = await fetch("https://animechan.vercel.app/api/random");
+        const rand_quote = await res.json();
+        return rand_quote
+    }
+
+    fetch_quote().then(res => console.log(res));
+    let quote = document.getElementById("quote");
+    fetch_quote().then(res => { quote.innerHTML = res.quote });
+    let show = document.getElementById("show");
+    fetch_quote().then(res => { show.innerHTML = res.anime });
 
     //function to auto-update current year
-     let current_year = () => {
+    let current_year = () => {
         return new Date().getFullYear();
     }
     let year = document.getElementById("year");
