7fc35f519adc42484e4a9b681666c1aa08941113
jim4067 <jimmyimpulse2@gmail.com>
2022-05-18 19:58:35 +0300

    update readme and links to blogit project

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 This repo houses my vanilla profile
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/7b209bd1-d054-4d1e-90b9-bf81cf73102d/deploy-status)](https://app.netlify.com/sites/distracted-mahavira-c4a1c1/deploys)
-
+[![cloudflare pages - success](https://img.shields.io/static/v1?label=cloudflare+pages&message=sucess&color=green&logo=blue&logoColor=yellow)](https://mutuku.me)
--- index.js
+++ index.js
@@ -5,6 +5,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
         "If I always blame someone or something, i'll never change - fruits basket"
     ]
 
+    //fetch the commits from this repo to get last_update
+    const getLatestCommit = async () => {
+        const commitsResp = await fetch("https://api.github.com/repos/jim4067/mutuku/commits");
+        return (await commitsResp.json())[0];
+    }
+
+    getLatestCommit().then(res => {
+        console.log("this is the first commit-> ", res);
+
+        //Awesome Date handling using JavaScript
+        const readableDate = new Date(res.commit.author.date);
+        console.log("this the readable date -> ", readableDate.toDateString());
+        // TODO -> Add this date to html for latest update of CV
+    }).catch(err => {
+        console.error("Error in trying to get the commits from this repo ->", err);
+    });
+
     //fetch a random page only when we are in the home page
     if (document.URL.includes("index.html")) {
         async function fetch_quote() {
